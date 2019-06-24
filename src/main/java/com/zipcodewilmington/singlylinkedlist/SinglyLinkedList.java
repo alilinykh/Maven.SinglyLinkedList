0077708842e9be1799ceb93a282020fb5cd79dbd
alilinykh <aililnykh@dtcc.edu>
2019-06-24 17:10:59 -0400

    done to copy

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,119 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    Node head;
+
+    public class Node {
+        private T data;
+        private Node next;
+    }
+
+    public void add(T data){
+
+        Node node = new Node();
+        node.data  = data;
+        node.next = null; //optional
+
+        if(head == null) {
+            head = node;
+        }
+        else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+    public Integer size() {
+        Integer counter = 1;
+        if (head == null) {
+            return null;
+        } else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public void removeByIndex(Integer index){
+        Node node = new Node();
+        node.next = null; //optional
+
+        if (index == 0) {
+            head = head.next;
+        }
+        else {
+            Node n = head;
+
+            for (int i = 0; i < index - 1; i++) {
+                n = n.next;
+            }
+            Node n1 = n.next;
+            n.next = n1.next;
+        }
+    }
+
+    public Boolean contains(T value){
+        if (head == null) {return false;}
+        else {
+            Node n = head;
+            while (n.next != null) {
+                if (n.data.equals(value)) {
+                    return true;
+                }
+                n = n.next;
+            }
+        }
+        return false;
+    }
+
+    public Integer find (T value){
+        Node node = new Node();
+        node.next = null; //optional
+        Integer counter = 0;
+
+            Node n = head;
+            while (n.next != null) {
+                if (n.data.equals(value)) {
+                    return counter;
+                }
+                n = n.next;
+                counter++;
+            }
+        return -1;
+    }
+
+    public T get(Integer index){
+        Node node = new Node();
+        node.next = null;
+        Node n = head;
+
+        if (index == 0) {
+            return n.data;
+        }
+        else {
+            for (int i = 0; i < index; i++) {
+                n = n.next;
+            }
+            return n.data;
+        }
+    }
+
+    public List<T> copy(SinglyLinkedList list){
+        return null;
+    }
+
+    public void sort(T value){}
+    //optional
+    public void reverse(T value){}
+    public void slice(T value){}
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,134 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList singlyLinkedList;
+    @Before
+    public void setUp() throws Exception {
+        singlyLinkedList = new SinglyLinkedList();
+    }
+
+    @Test
+    public void addTest() {
+        singlyLinkedList.add("String");
+        Integer expected = 1;
+        Integer actual = singlyLinkedList.size();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void sizeTest() {
+        singlyLinkedList.add(1);
+        singlyLinkedList.add(2);
+        singlyLinkedList.add(100);
+        Integer expected = 3;
+        Integer actual = singlyLinkedList.size();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void sizeTestNull() {
+        Integer expected = null;
+        Integer actual = singlyLinkedList.size();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void removeTest() {
+        singlyLinkedList.add("First Item");
+        singlyLinkedList.add("Second");
+        singlyLinkedList.add("Puppy");
+        singlyLinkedList.removeByIndex(1);
+        Integer expected = 2;
+        Integer actual = singlyLinkedList.size();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void containsTest() {
+        singlyLinkedList.add("String");
+        singlyLinkedList.add("another");
+
+        Assert.assertTrue(singlyLinkedList.contains("String"));
+    }
+
+    @Test
+    public void containsTest1() {
+        singlyLinkedList.add("String");
+        singlyLinkedList.add("another");
+
+        Assert.assertFalse(singlyLinkedList.contains("lalalal"));
+    }
+
+    @Test
+    public void findTest() {
+        singlyLinkedList.add("String");
+        singlyLinkedList.add("another");
+        singlyLinkedList.add("yaaaa");
+
+        Integer expected = 1;
+        Integer actual = singlyLinkedList.find("another");
+
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void findTest1() {
+        singlyLinkedList.add("String");
+        singlyLinkedList.add("another");
+        singlyLinkedList.add("yaaaa");
+
+        Integer expected = -1;
+        Integer actual = singlyLinkedList.find("lalala");
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getTest() {
+        singlyLinkedList.add("String");
+        singlyLinkedList.add("another");
+        singlyLinkedList.add("yaaaa");
+
+        String expected = "String";
+        String actual = singlyLinkedList.get(0).toString();
+
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void getTest1() {
+        singlyLinkedList.add("String");
+        singlyLinkedList.add("another");
+        singlyLinkedList.add("ya");
+
+        String expected = "ya";
+        String actual = singlyLinkedList.get(2).toString();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void copyTest() {
+        SinglyLinkedList test = new SinglyLinkedList();
+        test.add(1);
+        test.add(1);
+        test.add(1);
+
+        List actual = singlyLinkedList.copy(test);
+
+        Assert.assertEquals(test,actual);
+    }
 }
